03f30580dd15580c2672c35041294554503b915f
CC <zhiwen.cai@evenrealities.com>
2026-05-20 11:17:22 +0800

    更新游戏界面

--- src/ui/pages/game.ts
+++ src/ui/pages/game.ts
@@ -7,10 +7,10 @@ const SUDOKU_PUZZLE = [
   [5, 3, 4, 6, 7, 8, 9, 1, 2],
   [6, 7, 2, 1, 9, 5, 3, 4, 8],
   [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [8, 5, 9, 0, 6, 1, 4, 2, 3],
   [4, 2, 6, 8, 5, 3, 7, 9, 1],
   [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [9, 6, 1, 0, 3, 7, 2, 8, 4],
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
   [3, 4, 5, 2, 8, 6, 1, 7, 0],
 ];
@@ -91,12 +91,12 @@ const generateBoardText = (): string => {
       const isSelected = row === gameState.selectedCell.row && col === gameState.selectedCell.col;
 
       if (val === 0) {
-        rowText += isSelected ? ' > ' : '   ';
+        rowText += isSelected ? ' ▶ ' : '   ';
       } else {
         rowText += isSelected ? `>${val} ` : ` ${val} `;
       }
-
-      if ((col + 1) % 3 === 0 && col < 8) rowText += '||';
+//■ ▢ ▣ ▤ ▥
+      if ((col + 1) % 3 === 0 && col < 8) rowText += '■';
       else if (col < 8) rowText += '|';
     }
     text += rowText + '\n';
